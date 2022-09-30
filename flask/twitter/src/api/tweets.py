a93b158db99711a9558928ee412aea37a59fe294
Tuan Tran <hometran@gmail.com>
2022-09-30 10:51:41 -0700

    add code part 4

--- flask/twitter/src/api/tweets.py
+++ flask/twitter/src/api/tweets.py
@@ -1,5 +1,13 @@
-from flask import Blueprint
 from flask import Blueprint, jsonify, abort, request
 from ..models import Tweet, User, db
 
 bp = Blueprint('tweets', __name__, url_prefix='/tweets')
+
+
+@bp.route('', methods=['GET'])  # decorator takes path and list of HTTP verbs
+def index():
+    tweets = Tweet.query.all()  # ORM performs SELECT query
+    result = []
+    for t in tweets:
+        result.append(t.serialize())  # build list of Tweets as dictionaries
+    return jsonify(result)  # return JSON response
