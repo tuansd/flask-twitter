376bf385a89dc7cd925977b4ce5b333b45b7fe57
Tuan Tran <hometran@gmail.com>
2022-09-30 11:17:56 -0700

    finish CRUD part 4

--- flask/twitter/src/api/tweets.py
+++ flask/twitter/src/api/tweets.py
@@ -11,3 +11,38 @@ def index():
     for t in tweets:
         result.append(t.serialize())  # build list of Tweets as dictionaries
     return jsonify(result)  # return JSON response
+
+
+@bp.route('/<int:id>', methods=['GET'])
+def show(id: int):
+    t = Tweet.query.get_or_404(id)
+    return jsonify(t.serialize())
+
+
+@bp.route('', methods=['POST'])
+def create():
+    # req body must contain user_id and content
+    if 'user_id' not in request.json or 'content' not in request.json:
+        return abort(400)
+    # user with id of user_id must exist
+    User.query.get_or_404(request.json['user_id'])
+    # construct Tweet
+    t = Tweet(
+        user_id=request.json['user_id'],
+        content=request.json['content']
+    )
+    db.session.add(t)  # prepare CREATE statement
+    db.session.commit()  # execute CREATE statement
+    return jsonify(t.serialize())
+
+
+@bp.route('/<int:id>', methods=['DELETE'])
+def delete(id: int):
+    t = Tweet.query.get_or_404(id)
+    try:
+        db.session.delete(t)  # prepare DELETE statement
+        db.session.commit()  # execute DELETE statement
+        return jsonify(True)
+    except:
+        # something went wrong :(
+        return jsonify(False)
